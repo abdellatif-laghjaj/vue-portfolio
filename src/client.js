d9bb52a4300a2ed881cdeff99f437faad2e2a8a4
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-30 23:30:47 +0100

    reset

--- src/client.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from "@sanity/client";
-
-export default sanityClient({
-    projectId: "s6aeoip5", // find this at manage.sanity.io or in your sanity.json
-    dataset: "production", // this is from those question during 'sanity init'
-    useCdn: true,
-    apiVersion: "2021-03-25"
-});
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import router from './router'
 import App from './App.vue'
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+createApp(App).mount('#app')
