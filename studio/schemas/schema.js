e0cc6471c49a8c28b487803bba3d831869532fa3
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-30 21:02:12 +0100

    create skill

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -6,8 +6,8 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
 import blockContent from './blockContent'
-import category from './category'
-import post from './post'
+import category from './skill'
+import post from './project'
 import technlogy from './technlogy'
 
 // Then we give our schema to the builder and provide the result to Sanity
---
+++ studio/schemas/skill.js
@@ -0,0 +1,20 @@
+export default {
+    name: 'skill',
+    title: 'Skill',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'mainImage',
+            title: 'Main image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+    ],
+}
