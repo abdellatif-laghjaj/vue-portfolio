7b374b9b5f8f4f3ebcf3b128afbb84319974701f
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-01 02:17:09 +0100

    bug fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <div class="max-w-lg">
         <!-- Avatar -->
         <div class="avatar mb-6">
-          <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+          <div class="w-32 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
             <img src="https://placeimg.com/192/192/people"/>
           </div>
         </div>
---
+++ src/views/Skills.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container w-11/12 sm:w-3/5 mx-auto flex items-center justify-center flex-col">
+    <!-- Section Title -->
+    <div class="text-4xl font-bold my-3">
+      <span>My </span>
+      <span class="text-primary">Skills</span>
+    </div>
+
+    <!-- Section Content -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About"
+}
+</script>
+
+<style scoped>
+
+</style>
