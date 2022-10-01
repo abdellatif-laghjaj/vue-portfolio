1620a770fcc07e120dc81f62efb3ab2708eb0ba0
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-01 11:40:38 +0100

    create tabs for skills

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -7,13 +7,30 @@
     </div>
 
     <!-- Section Content -->
+    <div class="mt-3 w-11/12 sm:w-auto">
+      <div class="tabs tabs-boxed">
+        <a class="tab flex-1 h-12 font-bold">Tab 1</a>
+        <a class="tab flex-1 h-12 font-bold tab-active">Tab 2</a>
+        <a class="tab flex-1 h-12 font-bold">Tab 3</a>
+      </div>
+
+      <div class="tab-content w-full">
+        <div class="card bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "About"
-}
+<script setup>
+
 </script>
 
 <style scoped>
