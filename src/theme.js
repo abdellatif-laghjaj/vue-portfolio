afebea9d3fd9c36f01166bd3548a88a910494cd4
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-02 12:31:54 +0100

    save theme to localstorage

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,8 @@
 const theme_items = document.querySelectorAll('.theme-item');
 
+//load theme
+getTheme();
+
 theme_items.forEach((theme) => {
     theme.addEventListener("click", (e) => {
         //remove all active classes
@@ -14,5 +17,22 @@ theme_items.forEach((theme) => {
         const theme = e.target.closest('.theme-item').getAttribute('data-set-theme');
         console.log(theme);
         document.documentElement.setAttribute('data-theme', theme);
+
+        //save theme to local storage
+        saveTheme(theme);
     });
 });
+
+//save theme to local storage
+function saveTheme(theme) {
+    localStorage.setItem('theme', theme);
+}
+
+//get theme from local storage
+function getTheme() {
+    if (localStorage.getItem('theme') !== null) {
+        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
+    } else {
+        document.documentElement.setAttribute('data-theme', 'halloween');
+    }
+}
