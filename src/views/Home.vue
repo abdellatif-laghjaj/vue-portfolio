1c59553196fb288102935856c9a80cbc10ccc68d
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-02 20:38:34 +0100

    show modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"></path>
           </svg>
-          <span class="ml-2">Download CV</span>
+          <span class="ml-2">download resume</span>
         </label>
       </div>
     </div>
@@ -32,7 +32,7 @@
   <input type="checkbox" id="cv-modal" class="modal-toggle"/>
   <div class="modal modal-bottom sm:modal-middle">
     <div class="modal-box">
-      <h3 class="font-bold text-lg">Download CV</h3>
+      <h3 class="font-bold text-lg">Download Resume</h3>
       <p class="py-4">My resume is not ready yet, but you can contact me or come back later.</p>
       <div class="modal-action">
         <label for="cv-modal" class="btn">Ok, Got it!</label>
