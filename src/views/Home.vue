e0cbb9011736e67653364763fe1ca17575eb8a1f
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-02 14:58:26 +0100

    bug fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
-      <div class="max-w-lg mb-[50px] sm:mt-0">
+      <div class="max-w-lg mb-[80px] sm:mt-0">
         <!-- Avatar -->
         <div class="avatar mb-6">
           <div class="w-32 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
@@ -16,7 +16,7 @@
           <pre data-prefix=">" class="text-warning"><code>installing...</code></pre>
           <pre data-prefix=">" class="text-success"><code>Done!</code></pre>
         </div>
-        <button class="btn btn-primary">
+        <button class="btn btn-primary w-full sm:w-auto">
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"></path>
