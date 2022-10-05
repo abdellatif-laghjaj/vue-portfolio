84968e1b27fc4f2eb630aa21943f2464da254d28
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-05 23:57:37 +0100

    update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,14 +12,14 @@
         <!-- Name -->
         <h3 class="text-4xl font-bold flex items-center justify-center gap-2">
           <span>Abdellatif Laghjaj </span>
-          <svg class="w-9 h-9 mt-[10px]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <svg class="w-9 h-9 mt-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd"
                   d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                   clip-rule="evenodd"></path>
           </svg>
         </h3>
         <div class="mockup-code flex items-start flex-col my-3">
-          <pre data-prefix="$"><code>npm install @love.js </code></pre>
+          <pre data-prefix="$"><code>npm install <span class="text-success">@love.js</span> </code></pre>
           <pre data-prefix=">" class="text-warning"><code>installing...</code></pre>
           <pre data-prefix=">" class="text-error"><code>error: love not found</code></pre>
         </div>
