d0436b800656cf60ad97432b9f09138395d82a0a
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-06 00:21:19 +0100

    add social media

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -79,6 +79,52 @@
           </form>
         </div>
       </div>
+
+      <!-- Social Media -->
+      <h1 class="font-bold text-3xl">Folow me on</h1>
+      <div class="my-4 flex items-center justify-center flex-row gap-2">
+        <a :href="linkedin" target="_blank" rel="noopener noreferrer">
+          <svg xmlns="http://www.w3.org/2000/svg"
+               viewBox="0 0 24 24" stroke-width="1.5" class="w-8 h-8" fill="none" stroke="currentColor"
+               stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <rect x="4" y="4" width="16" height="16" rx="2"/>
+            <line x1="8" y1="11" x2="8" y2="16"/>
+            <line x1="8" y1="8" x2="8" y2="8.01"/>
+            <line x1="12" y1="16" x2="12" y2="11"/>
+            <path d="M16 16v-3a2 2 0 0 0 -4 0"/>
+          </svg>
+        </a>
+
+        <a :href="instagram" target="_blank" rel="noopener noreferrer">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" class="w-8 h-8" fill="none"
+               stroke="currentColor" stroke-linecap="round"
+               stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <rect x="4" y="4" width="16" height="16" rx="4"/>
+            <circle cx="12" cy="12" r="3"/>
+            <line x1="16.5" y1="7.5" x2="16.5" y2="7.501"/>
+          </svg>
+        </a>
+
+        <a :href="facebook" target="_blank" rel="noopener noreferrer">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" class="w-8 h-8" fill="none"
+               stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"/>
+          </svg>
+        </a>
+
+        <a :href="github" target="_blank" rel="noopener noreferrer">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" class="w-8 h-8" fill="none"
+               stroke="currentColor" stroke-linecap="round"
+               stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path
+                d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"/>
+          </svg>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -92,6 +138,10 @@ export default {
       email: "",
       subject: "",
       message: "",
+      linkedin: "https://www.linkedin.com/in/abdellatif-laghjaj/",
+      github: "https://github.com/abdellatif-laghjaj",
+      facebook: "https://www.facebook.com/abdelatif.laghjaj",
+      instagram: "https://www.instagram.com/abdellatif_laghjaj/",
     };
   },
   methods: {
