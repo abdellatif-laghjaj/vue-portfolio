ce20829f9ecb82f49b506d8764aaa5746bb163d2
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-06 23:50:38 +0100

    update ui

--- src/App.vue
+++ src/App.vue
@@ -33,10 +33,10 @@ setTimeout(() => {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap');
 
-#app {
-  font-family: 'Roboto', sans-serif;
+* {
+  font-family: 'Poppins', sans-serif;
 }
 
 .preloader {
