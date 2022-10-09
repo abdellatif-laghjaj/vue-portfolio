6ea7a13d97d4fac9be336732d88b6d1e0925f4fc
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-10-10 00:50:08 +0100

    add animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="hero min-h-screen bg-base-200">
+    <!-- Animated Background -->
+    <div class="area">
+      <ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
     <div class="hero-content text-center">
       <div class="max-w-lg mb-[80px] sm:mt-0">
         <!-- Avatar -->
@@ -21,9 +36,11 @@
         </h3>
         <div class="mockup-code flex items-start flex-col my-3">
           <pre data-prefix="$"><code>npm install <span class="text-success">@love.js</span> </code></pre>
-          <pre data-prefix=">" class="text-warning"><code>installing...<code>{{ installationPercentage }}</code></code></pre>
+          <pre data-prefix=">"
+            class="text-warning"><code>installing...<code>{{ installationPercentage }}</code></code></pre>
           <pre data-prefix=">" class="text-error" v-if="isInstalled"><code>error: love not found</code></pre>
-          <pre data-prefix="#" v-if="isInstalled"><code>try npm install <span class="text-secondary italic">-g</span> <span class="text-warning">@gf.js</span></code></pre>
+          <pre data-prefix="#"
+            v-if="isInstalled"><code>try npm install <span class="text-secondary italic">-g</span> <span class="text-warning">@gf.js</span></code></pre>
         </div>
         <label class="btn btn-primary modal-button w-full" for="cv-modal">
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -78,5 +95,121 @@ const startInstallation = () => {
 </script>
 
 <style scoped>
+.area {
+  background: transparent;
+  background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
+  width: 100%;
+  height: 100vh;
+}
 
+.circles {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.circles li {
+  position: absolute;
+  display: block;
+  list-style: none;
+  width: 20px;
+  height: 20px;
+  background: rgba(255, 255, 255, 0.2);
+  animation: animate 25s linear infinite;
+  bottom: -150px;
+
+}
+
+.circles li:nth-child(1) {
+  left: 25%;
+  width: 80px;
+  height: 80px;
+  animation-delay: 0s;
+}
+
+
+.circles li:nth-child(2) {
+  left: 10%;
+  width: 20px;
+  height: 20px;
+  animation-delay: 2s;
+  animation-duration: 12s;
+}
+
+.circles li:nth-child(3) {
+  left: 70%;
+  width: 20px;
+  height: 20px;
+  animation-delay: 4s;
+}
+
+.circles li:nth-child(4) {
+  left: 40%;
+  width: 60px;
+  height: 60px;
+  animation-delay: 0s;
+  animation-duration: 18s;
+}
+
+.circles li:nth-child(5) {
+  left: 65%;
+  width: 20px;
+  height: 20px;
+  animation-delay: 0s;
+}
+
+.circles li:nth-child(6) {
+  left: 75%;
+  width: 110px;
+  height: 110px;
+  animation-delay: 3s;
+}
+
+.circles li:nth-child(7) {
+  left: 35%;
+  width: 150px;
+  height: 150px;
+  animation-delay: 7s;
+}
+
+.circles li:nth-child(8) {
+  left: 50%;
+  width: 25px;
+  height: 25px;
+  animation-delay: 15s;
+  animation-duration: 45s;
+}
+
+.circles li:nth-child(9) {
+  left: 20%;
+  width: 15px;
+  height: 15px;
+  animation-delay: 2s;
+  animation-duration: 35s;
+}
+
+.circles li:nth-child(10) {
+  left: 85%;
+  width: 150px;
+  height: 150px;
+  animation-delay: 0s;
+  animation-duration: 11s;
+}
+
+@keyframes animate {
+  0% {
+    transform: translateY(0) rotate(0deg);
+    opacity: 1;
+    border-radius: 0;
+  }
+
+  100% {
+    transform: translateY(-1000px) rotate(720deg);
+    opacity: 0;
+    border-radius: 50%;
+  }
+}
 </style>
