dd3d90654b2ce389d7884f919014daa17c4e98d5
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2023-07-08 10:31:47 +0100

    update projects cards

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="container w-11/12 sm:w-full mx-auto flex items-center justify-center flex-col"
-  >
+  <div class="container w-11/12 sm:w-full mx-auto flex items-center justify-center flex-col">
     <!-- Section Title -->
     <div class="text-4xl font-bold my-3">
       <span>My </span>
@@ -11,104 +9,49 @@
     <!-- Section Content -->
     <div class="skills-content">
       <p class="skills-text text-center">
-        Here are some of my <span class="text-secondary">projects</span> that
-        I've been working on.
+        Here are some of my <span class="text-secondary">projects</span> that I've
+        been working on.
       </p>
 
       <!-- Filter -->
       <div class="flex items-center justify-center">
-        <div
-          class="tabs tabs-boxed flex items-center justify-between w-[100%] sm:w-80 mt-5"
-        >
-          <a
-            class="tab h-12 font-bold tab-active"
-            @click="filterProjects('all')"
-            data-cat="all"
-            >All</a
-          >
-          <a
-            class="tab h-12 font-bold"
-            @click="filterProjects('web')"
-            data-cat="web"
-            >Web</a
-          >
-          <a
-            class="tab h-12 font-bold"
-            @click="filterProjects('mobile')"
-            data-cat="mobile"
-            >Mobile</a
-          >
-          <a
-            class="tab h-12 font-bold"
-            @click="filterProjects('desktop')"
-            data-cat="desktop"
-            >Desktop</a
-          >
+        <div class="tabs tabs-boxed flex items-center justify-between w-[100%] sm:w-80 mt-5">
+          <a class="tab h-12 font-bold tab-active" @click="filterProjects('all')" data-cat="all">All</a>
+          <a class="tab h-12 font-bold" @click="filterProjects('web')" data-cat="web">Web</a>
+          <a class="tab h-12 font-bold" @click="filterProjects('mobile')" data-cat="mobile">Mobile</a>
+          <a class="tab h-12 font-bold" @click="filterProjects('desktop')" data-cat="desktop">Desktop</a>
         </div>
       </div>
 
-      <div class="projects flex flex-wrap -m-4 mt-4" v-if="loaded">
-        <div
-          class="xl:w-1/3 md:w-1/2 p-4 project"
-          v-for="project in filtredProjects"
-          :key="project.id"
-        >
+      <!-- Projects -->
+      <div class="projects flex flex-wrap -m-4 mt-4 bg-slate-600 rounded-md" v-if="loaded">
+        <div class="xl:w-1/3 md:w-1/2 p-4 project" v-for="project in filtredProjects" :key="project.id">
           <div class="image">
-            <img :src="project.image" alt="project image" />
+            <img :src="project.image" alt="project image">
             <div class="details">
               <h2>{{ project.title }}</h2>
               <p>{{ makeTextShort(project.description, 80) }}</p>
-              <div
-                class="tags flex gap-2 items-center justify-center flex-wrap"
-              >
-                <span
-                  class="badge badge-secondary"
-                  v-for="tag in project.tags"
-                  :key="tag"
-                  >{{ tag }}</span
-                >
+              <div class="tags flex gap-2 items-center justify-center flex-wrap">
+                <span class="badge badge-secondary" v-for="tag in project.tags" :key="tag">{{ tag }}</span>
               </div>
               <div class="more">
                 <a class="read-more badge badge-primary font-bold py-2">
                   {{ project.category }}
                 </a>
                 <div class="icon-links flex flex-row items-center gap-1">
-                  <a
-                    :href="project.demo"
-                    target="_blank"
-                    class="bg-accent p-2 rounded-full"
-                    v-if="project.has_demo"
-                  >
-                    <svg
-                      class="w-6 h-6"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
+                  <a :href="project.demo" target="_blank" class="bg-accent p-2 rounded-full" v-if="project.has_demo">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                       <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
-                      <path
-                        fill-rule="evenodd"
-                        d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                        clip-rule="evenodd"
-                      ></path>
+                      <path fill-rule="evenodd"
+                            d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                            clip-rule="evenodd"></path>
                     </svg>
                   </a>
-                  <a
-                    :href="project.link"
-                    target="_blank"
-                    class="bg-secondary p-2 rounded-full"
-                  >
-                    <svg
-                      class="w-6 h-6"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                      ></path>
+                  <a :href="project.link" target="_blank" class="bg-secondary p-2 rounded-full">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd"
+                            d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
                     </svg>
                   </a>
                 </div>
@@ -128,13 +71,13 @@
 import api from "../api.js";
 
 export default {
-  name: "Projects",
+  name: 'Projects',
   data() {
     return {
       projects: [],
       filtredProjects: [],
       loaded: false,
-    };
+    }
   },
   methods: {
     getProjects() {
@@ -151,70 +94,28 @@ export default {
       });
 
       //add active class to the tab that contains category data attribute
-      document
-        .querySelector(`[data-cat=${category}]`)
-        .classList.add("tab-active");
+      document.querySelector(`[data-cat=${category}]`).classList.add("tab-active");
 
       if (category === "all") {
         this.getProjects();
       } else {
-        this.filtredProjects = this.projects.filter(
-          (project) => project.category === category
-        );
+        this.filtredProjects = this.projects.filter((project) => project.category === category);
       }
     },
     makeTextShort(text, maxLength) {
       if (text.length > maxLength) {
-        return text.substr(0, maxLength) + "...";
+        return text.substr(0, maxLength) + '...'
       }
-      return text;
-    },
+      return text
+    }
   },
   created() {
     this.getProjects();
-  },
-};
+  }
+}
 </script>
 
 <style scoped>
-/* Add this CSS to your stylesheet or style tag */
-
-.project {
-  position: relative;
-}
-
-.project .image {
-  position: relative;
-}
-
-.project .image:before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(255, 255, 255, 0.2); /* Adjust the transparency as needed */
-  opacity: 0;
-  transition: opacity 0.3s ease;
-}
-
-.project:hover .image:before {
-  opacity: 1;
-}
-
-.project .details {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  color: #fff; /* Set the text color to your desired color */
-  z-index: 1;
-}
-
-/* Add any other styles as needed */
-
 .image {
   position: relative;
   flex: 1;
